59a2323391c228be89fe5e7a3b9b9750ffc57b59
RJCreation-source <thakurroshan1021@gmail.com>
2026-08-25 04:51:25 +0530

    Update page title in Streamlit configuration

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-st.set_page_config(page_title="E-Commerce Analytics PRO", layout="wide", page_icon="📊")
+st.set_page_config(page_title="E-Commerce Analytics ", layout="wide", page_icon="📊")
 st.title("📊 E-Commerce Sales Analytics - PRO Version")
 
 @st.cache_data
